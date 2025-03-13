060544d017be95f6e9b8b8d3b83ed413fb1d8912
DivZeroException <52114336+PotacioEnBarra@users.noreply.github.com>
2025-03-13 13:22:38 -0500

    Video section added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <HeroSection />
   <CounterSection />
+  <VideoSection />
   <UsersSection />
   <FormSection/>
   <FooterSection />
@@ -12,13 +13,16 @@ import CounterSection from './components/CounterSection.vue';
 import FormSection from './components/FormSection.vue';
 import FooterSection from './components/layouts/FooterSection.vue';
 import UsersSection from './components/UsersSection.vue';
+import VideoSection from './components/VideoSection.vue';
 export default {
   name: 'App',
   components: {
     HeroSection,
     CounterSection,
+    VideoSection,
     UsersSection,
     FormSection,
+    
     FooterSection
   }
 }
---
+++ src/components/VideoSection.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="video">
+        <div class="video__container">
+            <img src="@/assets/images/video.png" alt="">
+            <div class="video__container--decorator">
+                <div class="video__icon">
+                    <img src="@/assets/images/Arrow-3.png" alt="Imagen Estática" />
+                </div>
+                <div class="video__text">
+                    <p>Ready to</p>
+                    <p>get started</p>
+                </div>
+            </div>
+            <div class="video__container--decorator type-02">
+                <div class="video__text type-02">
+                    <p>Ready to</p>
+                    <p>get started</p>
+                </div>
+                <div class="video__icon">
+                    <img src="@/assets/images/Arrow-4.png" alt="Imagen Estática" />
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
+<script>
+export default {
+    name: 'VideoSection'
+}
+</script>
+<style lang="scss">
+@use '@/assets/styles/components/video.scss';
+</style>
